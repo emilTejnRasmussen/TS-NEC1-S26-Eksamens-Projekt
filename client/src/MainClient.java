e32a0601a2eb7cc15ac735a744148a62650d0ee6
Emil Tejn Rasmussen <emil@rasie.dk>
2026-04-15 11:59:58 +0200

    Make better test main

--- client/src/MainClient.java
+++ client/src/MainClient.java
@@ -1,46 +1,44 @@
 import socket.ClientSocketManager;
 import socket.json.ClientType;
-ClientSocketManager socket;
 
-void main() {
-    socket = new ClientSocketManager("localhost", 6789);
-    boolean running = true;
+import java.util.Scanner;
+
+ClientSocketManager sensor1;
+ClientSocketManager light1;
+ClientSocketManager display1;
+
+void main()
+{
+    sensor1 = new ClientSocketManager("localhost", 6789);
+    light1 = new ClientSocketManager("localhost", 6789);
+    display1 = new ClientSocketManager("localhost", 6789);
+
+    sensor1.register("sensor-1", 1, ClientType.SENSOR);
+    light1.register("light-1", 1, ClientType.LIGHT);
+    display1.register("display-1", null, ClientType.DISPLAY);
+
     Scanner input = new Scanner(System.in);
+    boolean running = true;
 
-    createClients();
-
-    while (running) {
+    while (running)
+    {
         System.out.print("> ");
         String line = input.nextLine();
 
-        switch (line) {
-            case "reg client" -> socket.register("sensor-1", 1, ClientType.SENSOR);
+        switch (line)
+        {
+            case "park" -> sensor1.carParked("sensor-1", 1);
+            case "leave" -> sensor1.carLeft("sensor-1", 1);
+            case "heartbeat sensor" -> sensor1.heartbeat("sensor-1", 1, ClientType.SENSOR);
+            case "heartbeat light" -> light1.heartbeat("light-1", 1, ClientType.LIGHT);
+            case "heartbeat display" -> display1.heartbeat("display-1", null, ClientType.DISPLAY);
+            case "quit" -> running = false;
+            default -> System.out.println(
+                    "Commands: park, leave, heartbeat sensor, heartbeat light, heartbeat display, quit");
         }
     }
-    socket.disconnect();
-}
 
-private void createClients()
-{
-    for (int i = 1; i <= 5; i++)
-    {
-        createSensor(i);
-        createLight(i);
-    }
-    createDisplay();
-}
-
-private void createSensor(int i)
-{
-    socket.register("sensor-" + i, i, ClientType.SENSOR);
-}
-
-private void createLight(int i)
-{
-    socket.register("light-" + i, i, ClientType.LIGHT);
-}
-
-private void createDisplay()
-{
-    socket.register("display-" + 1, null, ClientType.LIGHT);
+    sensor1.disconnect();
+    light1.disconnect();
+    display1.disconnect();
 }
